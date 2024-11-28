1b40a4ec22c82995d6c81ce1d130e84aa7a001fa
saba696 <sisaurisaba8@gmail.com>
2024-11-28 23:08:15 +0400

    commit

---
+++ lvl16/hm/hm5.py
@@ -0,0 +1,16 @@
+
+wona = int(input("ჩაწერე წონა"))
+
+if wona < 10:
+    print("შენთვის არ შეიძლება ამის მირება")
+
+
+elif wona >= 10 and wona <= 20:
+    print("მიირიე ნახევარი ტაბლეტი")
+
+
+elif wona >= 30 and wona <= 40:
+    print("უნდა მიირო ერტი ტაბლეტი")
+
+elif wona >= 54:
+    print("მოორეთ 3 ტაბლეტი დღეში")
---
+++ lvl16/hm/hm6.py
@@ -0,0 +1,8 @@
+kurtka = input("acvia?: ")
+fexsacmeli = input("acvia?: ")
+ 
+if kurtka == "True" and fexsacmeli == "True" :
+    print("შეგიძლია შესვლა")
+
+else:
+    print("არ შეგიძლია შესვლა")
